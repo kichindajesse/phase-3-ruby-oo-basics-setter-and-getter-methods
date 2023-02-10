2ee3f986f3385be6b811a4a8ba58c7b3119916a8
kichinda <53686544+kichinda@users.noreply.github.com>
2023-02-10 21:27:23 +0300

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+  def name=(name)
+    @name = name
+  end
 
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
 end
+
+person = Person.new
+person.name = "John Doe"
--- spec/ruby_object_attributes_spec.rb
+++ spec/ruby_object_attributes_spec.rb
@@ -93,3 +93,4 @@ describe 'Person' do
     end
   end
 end
+
